5bb3730d89b80b3569da7fef2722f7134ad41800
HP VICTUS <egorkrauz5@gmail.com>
2024-12-13 16:34:44 +0200

    init

---
+++ DZ_3.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_info(self):
+        return f"{self.year} {self.make} {self.model}"
+
+car = Car("Mitsubishi", "Lancer", 2010)
+print(car.get_info())
